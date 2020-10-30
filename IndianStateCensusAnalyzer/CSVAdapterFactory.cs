f914e7a2f9c5f33ed59a4c5d0f53d1b73dc753cf
Saurabh <saurabh.nit20@gmail.com>
2020-10-31 03:30:38 +0530

    [Saurabh].Load State Code Info from CSV

--- IndianStateCensusAnalyzer/CSVAdapterFactory.cs
+++ IndianStateCensusAnalyzer/CSVAdapterFactory.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IndianStateCensusAnalyzer.DTO;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,17 @@ namespace IndianStateCensusAnalyzer
 {
     class CSVAdapterFactory
     {
+        public Dictionary<string, CensusDTO> LoadCsvData(CensusAnalyser.Country country, string csvFilePath, string dataHeaders)
+        {
+            switch (country)
+            {
+                case (CensusAnalyser.Country.INDIA):
+                    return new IndianCensusAdapter().LoadCensusData(csvFilePath, dataHeaders);
+                //case (CensusAnalyser.Country.US):
+                //    return new USCensusAdapter().LoadUSCensusData(csvFilePath, dataHeaders);
+                default:
+                    throw new CensusAnalyserException("No Such Country", CensusAnalyserException.ExceptionType.NO_SUCH_COUNTRY);
+            }
+        }
     }
 }
--- IndianStateCensusAnalyzer/Program.cs
+++ IndianStateCensusAnalyzer/Program.cs
@@ -2,7 +2,7 @@
 
 namespace IndianStateCensusAnalyzer
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
